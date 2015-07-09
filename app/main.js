7098a10c705f2a39e66ab974ed0be53c866b57f1
ibjohansen <ib.johansen@inmeta.com>
2015-07-09 13:49:51 +0200

    added skeleton

--- app/main.js
+++ app/main.js
@@ -6,7 +6,8 @@ import Home from './pages/home';
 import Map from './pages/map';
 import Cats from './pages/cats';
 
-require("./styles/reset.css");
+require("./styles/normalize.css");
+require("./styles/skeleton.css");
 require("./styles/app.css");
 
 React.initializeTouchEvents(true);
@@ -25,9 +26,7 @@ let routes = (
 var App = React.createClass({
     render () {
         return (
-            <div>
-                <RouteHandler/>
-            </div>
+            <RouteHandler/>
         )
     }
 });
--- app/pages/home/index.js
+++ app/pages/home/index.js
@@ -16,15 +16,25 @@ module.exports = React.createClass({
 
     render() {
         return (
-            <div>
-                <Header/>
+            <div className="container">
+                <div className="row">
+                    <div className="twelve columns">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="twelve columns">
+                        <main role="main">
+                            <h1>{this.props.title}</h1>
 
-                <main role="main">
-                    <h1>{this.props.title}</h1>
+                            <h3>{this.props.body}</h3>
 
-                    <h3>{this.props.body}</h3>
-                    <img src={require('../../images/top1.jpg')} alt=""/>
-                </main>
+
+                            <img className="u-max-full-width" src={require('../../images/top1.jpg')} alt=""/>
+                        </main>
+
+                    </div>
+                </div>
             </div>
         )
     }
